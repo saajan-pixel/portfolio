a5a0629722013a4b5785c75c77ca558d0b9024c8
saajan-pixel <rcccstha@gmail.com>
2022-09-25 10:10:40 +0545

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './app.scss'
 import Topbar from "./components/topbar/Topbar";
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Works from './components/works/Works'
+// import Testimonials from './components/testimonials/Testimonials';
+// import Works from './components/works/Works'
 import Contact from './components/contact/Contact'
 import { useState } from 'react';
 import Menu from './components/menu/Menu';
