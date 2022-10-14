6ac4684a9bd510e53943fb37679916ed827b1d4e
saajan-pixel <rcccstha@gmail.com>
2022-10-14 20:21:29 +0545

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./app.scss";
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
-import useGaTracker from "./useGaTracker";
 // import Testimonials from './components/testimonials/Testimonials';
 // import Works from './components/works/Works'
 import Contact from "./components/contact/Contact";
